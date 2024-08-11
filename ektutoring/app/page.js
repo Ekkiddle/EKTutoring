63b56c45de0849b321667d66ff42ae6699830f07
Ekkiddle <ekkiddle@gmail.com>
2024-08-11 16:39:37 -0600

    Add contact info

--- ektutoring/app/page.js
+++ ektutoring/app/page.js
@@ -2,12 +2,21 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Subjects from "./components/Subjects";
 import Footer from "./components/Footer";
+import { MdOutlineMailOutline, MdPhone } from "react-icons/md";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex w-screen min-h-screen flex-col bg-indigo-100">
-      <div className="w-screen h-10 bg-black">
+      <div className="flex flex-row justify-end items-center w-screen h-10 bg-black px-10 space-x-10">
+        <div className="flex flex-row items-center my-3 text-gray-500 hover:text-gray-400">
+          <MdOutlineMailOutline className="text-2xl mx-3"/>
+          <a href="mailto:ektutoring@gmail.com" className="text-sm hover:underline">ektutoring@gmail.com</a>
+        </div>
+        <div className="flex flex-row items-center my-3 text-gray-500 hover:text-gray-400">
+          <MdPhone className="text-2xl mx-3"/>
+          <a href="tel:+15877037826" className="text-sm hover:underline">587-703-7826</a>
+        </div>
       </div>
       <Navbar />
       <div className="flex flex-col items-center justify-between">
@@ -16,8 +25,8 @@ export default function Home() {
           <div className="w-full h-20 bg-indigo-900 shadow-xl" ></div>
           <div className="text-indigo-900 bg-indigo-300 px-40 py-10" >
             <div className="p-10 bg-indigo-200 rounded-md flex flex-col">
-              <h1 className="text-3xl text-bold mt-5 mb-1 ">Personalized Tutoring Services in NW Calgary</h1>
-              <h2 className="text-xl text-bold italic mb-5">Empowering Students with Tailored Educational Support</h2>
+              <h1 className="text-3xl font-bold mt-5 mb-1 ">Personalized Tutoring Services in NW Calgary</h1>
+              <h2 className="text-xl font-bold italic mb-5">Empowering Students with Tailored Educational Support</h2>
               <p className="py-2">
               Centered in North West Calgary Communities, EKTutoring is a <b>self-run </b>business created by 
               Emily Kiddle focused on <b>helping students achieve academic success. </b>
